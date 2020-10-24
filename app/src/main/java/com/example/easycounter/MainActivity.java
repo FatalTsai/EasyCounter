a3ab457795e2aa6b7bfd3afb22c6ddc256314e38
蔡昀辰 Cheney.Tsai <Cheney.Tsai@jet-opto.com.tw>
2020-10-25 04:14:30 +0800

    fuck update for lingshishiou

--- app/src/main/java/com/example/easycounter/MainActivity.java
+++ app/src/main/java/com/example/easycounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.easycounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,14 +14,18 @@ public class MainActivity extends AppCompatActivity
     TextView txv;
     Button btn;
     int counter = 0;
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
-        @Override
+
+
+    @Override
         protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txv =(TextView) findViewById(R.id.textView);
-        btn = (Button) findViewById(R.id.button);
+        btn = (Button) findViewById(R.id.add);
 
         btn.setOnClickListener(this);
         btn.setOnLongClickListener(this);
@@ -32,6 +37,14 @@ public class MainActivity extends AppCompatActivity
         txv.setText(String.valueOf(++counter));
     }
 
+
+    public void minus(View v){
+        txv.setText(String.valueOf(--counter));
+    }
+
+    public  void finish(View v){
+        MainActivity.this.finish();
+    }
 
     @Override
     public boolean onLongClick(View v) {
@@ -45,6 +58,44 @@ public class MainActivity extends AppCompatActivity
             txv.setText(String.valueOf(counter));
         }
         return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    //ref from:https://stackoverflow.com/questions/6645537/how-to-detect-the-swipe-left-or-right-in-android?fbclid=IwAR2lQ5dvUe3XrpaEcyyJW9HmNfhcagynlLTsB-1OGf1t17aVr3XNIrgdSzU
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    if (x2 > x1)
+                    {
+                        counter-=2;
+                        txv.setText(String.valueOf(counter));
+
+                    }
+
+                    // Right to left swipe action
+                    else
+                    {
+                        counter+=2;
+                        txv.setText(String.valueOf(counter));
+                    }
+
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
 }
